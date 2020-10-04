54d7afd7c0fc9ab472c44c90c235362bdda5191a
Elisandro Moreira <elisandro.moreira@univille.br>
2020-10-04 04:36:37 -0300

    Project started

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ca880af701fd6d705870fb3569158c8'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2739bf70-0418-4470-b0b7-33d84112ef2f'
+};
